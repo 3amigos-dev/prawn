746a6a83eb71ec9a8706decd1457cb2d19eb761b
Tim Gates <tim.gates@iress.com>
2019-12-19 16:43:45 +1100

    remove test

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,16 +4,11 @@ Test modules for:
 prawn.__main__
 """
 
-from prawn.__main__ import main
-
 
 def test_main():
     """
-    GIVEN the .__main__ module entry point WHEN calling main THEN the call
-    executes successfully with a result of `None`
+    Empty test
     """
     # Setup
     # Exercise
-    result = main()  # pylint: disable=assignment-from-no-return
     # Verify
-    assert result is None  # nosec
